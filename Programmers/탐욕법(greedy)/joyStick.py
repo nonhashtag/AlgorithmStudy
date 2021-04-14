6122caa9f4f0375b6d9c2c313f6ed4557d195a60
nonhashtag <75101880+nonhashtag@users.noreply.github.com>
2021-04-14 13:29:21 +0900

    Update joyStick.py
    단순 계산이 아니라, 좌우로 거리계산 후 이동하는 방식을 택함으로써 통과못한 테스트케이스 문제도 해결함.

--- Programmers/탐욕법(greedy)/joyStick.py
+++ Programmers/탐욕법(greedy)/joyStick.py
@@ -1,22 +1,19 @@
 def solution(name):
-    answer = 0
-    AtoN=[i for i in range(65,79)]
-    MtoZ=list(reversed([i for i in range(79,91)]))
+    loc=0
+    AtoZ = [min(ord(i)-ord("A"), (ord("Z")-ord(i))+1) for i in name]
     
-    if name=="A":
-        return 0
-    
-    if len(name)>=2:
-        for i in range(1, len(name)):
-            if name[i]=="A":
-                answer-=1
-            else:
-                break
-            
-    for i in name:
-        if ord(i) in AtoN:
-            answer += ord(i)-65
-        elif ord(i) in MtoZ:
-            answer += MtoZ.index(ord(i))+1
-    
-    return answer+len(name)-1
+    answer=sum(AtoZ)
+    while True:
+        AtoZ[loc]=0
+        if sum(AtoZ)==0:
+            return answer
+        left, right = 1, 1
+        while AtoZ[loc-left]==0:
+            left+=1
+        while AtoZ[loc+right]==0:
+            right+=1
+        answer+= left if left<right else right
+        loc+= -left if left<right else right
+        
+        
+print(solution("BBAAAAAB"))
