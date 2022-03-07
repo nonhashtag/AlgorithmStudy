94d666c06ff5610460ea1634c3c544310ccaedd0
nonhashtag <75101880+nonhashtag@users.noreply.github.com>
2022-03-07 23:53:51 +0900

    Create (15649번)N과M(1).py

---
+++ baekjoon/백트래킹/(15649번)N과M(1).py
@@ -0,0 +1,17 @@
+#수열
+def permutation(n, m, words):
+    if len(words) == m:
+        print(" ".join(list(map(str, words))))
+        return
+
+    for i in range(1, n+1):
+        if i not in words:
+            words.append(i)
+        else:
+            continue
+        permutation(n, m, words)
+        words.pop()
+
+N, M = map(int, input().split())
+
+permutation(N,M,[])
