e2a8736d7c978dac4522663c5781167eb14f9e82
nonhashtag <75101880+nonhashtag@users.noreply.github.com>
2022-03-26 01:58:00 +0900

    Update (1644번)소수의연속.py

--- baekjoon/투포인터/(1644번)소수의연속.py
+++ baekjoon/투포인터/(1644번)소수의연속.py
@@ -1,29 +1,19 @@
 import sys
 
-# 소수리스트 생성과정에서 시간초과 발생.
-# 에라토스 테네스 알고리즘을 참조할 것
-
-def isPrime(N):
-    if N==1:
-        return 0
-    if N==2:
-        return 1
-    elif N==3:
-        return 1
-    for i in range(2, int(N**0.5)+2):
-        if N % i == 0:
-            return 0
-    return 1
-
-def primes(arr, N):
-    for i in range(1, N+1):
-        if isPrime(i)==1:
-            arr.append(i)
+# 에라스토테네스의 체
+def prime_list(n):
+    sieve = [True]*(n+1)
+    m = int(n**0.5)
+    for i in range(2, m+1):
+        if sieve[i]==True:
+            for j in range(i*i, n+1, i):
+                sieve[j]=False
+    return [i for i in range(2, N+1) if sieve[i]==True]
 
 def search(arr, N):
     start, end = 0, 0
     answer=0
-    summary = arr[start]
+    summary = arr[start] if len(arr)>0 else 0
     while end<len(arr):
         if summary<N:
             end+=1
@@ -38,7 +28,8 @@ def search(arr, N):
             if end<len(arr):
                 summary += arr[end]
     return answer
+
 N = int(sys.stdin.readline())
-arr = []
-primes(arr, N)
+arr = prime_list(N)
+
 print(search(arr, N))
