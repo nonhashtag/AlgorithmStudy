225a898476cd64e5af280ee42c23ea33033d88b0
nonhashtag <75101880+nonhashtag@users.noreply.github.com>
2022-03-31 23:51:33 +0900

    Update (2606번)바이러스.py
    BFS 추가됨

--- baekjoon/DFS와BFS/(2606번)바이러스.py
+++ baekjoon/DFS와BFS/(2606번)바이러스.py
@@ -7,6 +7,16 @@ def dfs(graph, cur, visited):
         if visited[i]==0:
             dfs(graph, i, visited)
 
+def bfs(graph, q, visited):
+    visited[q[0]]=1
+    while len(q)>0:
+        cur = q.pop(0)
+
+        for i in graph[cur]:
+            if visited[i]==0:
+                q.append(i)
+                visited[i]=1
+
 verts = int(sys.stdin.readline())
 edges = int(sys.stdin.readline())
 graph = [[] for _ in range(verts+1)]
@@ -17,7 +27,8 @@ for i in range(edges):
     graph[a].append(b)
     graph[b].append(a)
 
-dfs(graph, 1, visited)
+#dfs(graph, 1, visited)
+bfs(graph, [1], visited)
 answer = 0
 for i in visited:
     if i==1:
