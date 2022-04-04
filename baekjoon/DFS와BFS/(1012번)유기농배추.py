c73d4ebe62581b0b595f69c331fa5dfd8aa2ceb4
nonhashtag <75101880+nonhashtag@users.noreply.github.com>
2022-04-04 20:33:57 +0900

    Update (1012번)유기농배추.py
    bfs 추가함

--- baekjoon/DFS와BFS/(1012번)유기농배추.py
+++ baekjoon/DFS와BFS/(1012번)유기농배추.py
@@ -1,5 +1,6 @@
 import sys
 
+# 기본 재귀호출횟수로는 부족함..
 sys.setrecursionlimit(10000)
 
 def dfs(graph, y, x, h, w):
@@ -12,6 +13,22 @@ def dfs(graph, y, x, h, w):
         if 0<=nextX and nextX<w and 0<=nextY and nextY<h:
             if graph[nextY][nextX]==1:
                 dfs(graph, nextY, nextX, h, w)
+
+def bfs(graph, y, x, h, w):
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, -1, 1]
+    q = [(y, x)]
+    while len(q)>0:
+        y1, x1 = q.pop(0)
+
+        for i in range(4):
+            nextX = x1 + dx[i]
+            nextY = y1 + dy[i]
+            if nextY>=0 and nextX>=0 and nextX<w and nextY<h:
+                if graph[nextY][nextX]==1:
+                    graph[nextY][nextX]=0
+                    q.append((nextY, nextX))
+
 
 T = int(sys.stdin.readline())
 
@@ -28,7 +45,8 @@ for tc in range(T):
     for i in range(N):
         for j in range(M):
             if graph[i][j]==1:
-                dfs(graph, i, j, N, M)
+                #dfs(graph, i, j, N, M)
+                bfs(graph, i, j, N, M)
                 cnt+=1
     print(cnt)
 
