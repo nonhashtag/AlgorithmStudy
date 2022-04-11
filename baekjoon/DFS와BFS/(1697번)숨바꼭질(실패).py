d37c00f8fec370829116e73dcfcb1b82db12f4d8
nonhashtag <75101880+nonhashtag@users.noreply.github.com>
2022-04-12 02:35:21 +0900

    Update (1697번)숨바꼭질(실패).py

--- baekjoon/DFS와BFS/(1697번)숨바꼭질(실패).py
+++ baekjoon/DFS와BFS/(1697번)숨바꼭질(실패).py
@@ -1,24 +1,25 @@
 import sys
+from collections import deque
 
 def bfs(q, visited, target):
     walk = [-1, 1, 0]
     tel = [1, 1, 2]
     cnt=0
     while len(q)>0:
-        cnt+=1
         for n in range(len(q)):
-            cur = q[n]
+            cur = q.popleft()
             if cur==target:
                 return cnt
             visited[cur]=1
-            q.pop(0)
             for i in range(3):
                 nextX = (cur + walk[i]) * tel[i]
                 if 0 <= nextX and nextX <= 100000:
                     if visited[nextX]==0:
                         q.append(nextX)
+        cnt += 1
 
 N, K = map(int, sys.stdin.readline().split())
 visited = [0]*100001
-q = [N]
+q = deque()
+q.append(N)
 print(bfs(q, visited, K))
