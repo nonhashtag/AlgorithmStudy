bcf8b21f4ae797e0de8e83bce6dbb5a39bb8bbca
nonhashtag <75101880+nonhashtag@users.noreply.github.com>
2022-04-13 23:54:10 +0900

    Update (1202번)보석도둑(실패).cpp

--- baekjoon/그리디알고리즘/(1202번)보석도둑(실패).cpp
+++ baekjoon/그리디알고리즘/(1202번)보석도둑(실패).cpp
@@ -8,30 +8,66 @@
 
 using namespace std;
 
-unsigned long long picking(vector <int> bags, vector <pair<int, int>> jewels, int K)
+unsigned long long picking(vector <int> bags, vector <pair<int, int>> jewels, int N, int K)
 {
 	priority_queue <int> candidate;
-	int cur = 0;
 	int idx = 0;
+	int valid = 0;
+	int b = 0;
 	unsigned long long answer = 0;
 
-	for(int b : bags)
+	for(int i=0; i<K; i++)
 	{
-		while (idx < K)
+		if (idx >= N)
+			break;
+		b = bags[0];
+		bags.erase(bags.begin());
+		if (b >= jewels[idx].first)
 		{
-			if (b < jewels[idx].first)
-				break;
-			candidate.push(jewels[idx].second);
-			idx += 1;
+			valid += 1;
+			while (idx < N)
+			{
+				if (b < jewels[idx].first)
+					break;
+				candidate.push(jewels[idx].second);
+				idx += 1;
+			}
+		}
+		/*
+		반례
+		3 3
+		2 100
+		4 42
+		4 500
+		2
+		3
+		4
+		정답	: 600
+		오출력	: 642 
+		*/
+		else if (i > 0 && !candidate.empty())
+		{
+			answer += candidate.top();
+			candidate.pop();
 		}
 	}
-	for (int i = 0; i < K; i++)
+
+
+	for (int i = 0; i < valid; i++)
 	{
 		answer += candidate.top();
 		candidate.pop();
 	}
+	for (int i : bags)
+	{
+		if (candidate.empty())
+			break;
+		answer += candidate.top();
+		candidate.pop();
+	}
+
 	return answer;
-	
+
 }
 
 int main()
@@ -58,7 +94,7 @@ int main()
 
 	sort(bags.begin(), bags.end());
 	sort(jewels.begin(), jewels.end());
-	answer = picking(bags, jewels, K);
+	answer = picking(bags, jewels, N, K);
 
 	/*
 	for (int i : bags)
