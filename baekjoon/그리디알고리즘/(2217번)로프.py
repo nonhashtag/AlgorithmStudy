0263f00f084898ff1c19ddc717219e33c58bfa66
nonhashtag <75101880+nonhashtag@users.noreply.github.com>
2022-04-19 20:08:23 +0900

    Update and rename (2217번)로프(실패).py to (2217번)로프.py

--- baekjoon/그리디알고리즘/(2217번)로프.py
+++ baekjoon/그리디알고리즘/(2217번)로프.py
@@ -5,13 +5,16 @@ N = int(sys.stdin.readline())
 ropes = []
 
 for i in range(N):
+    #ropes.append(int(sys.stdin.readline()))
     heapq.heappush(ropes, int(sys.stdin.readline()))
+#ropes.sort()
 
 answer = len(ropes) * ropes[0]
 heapq.heappop(ropes)
 for i in range(len(ropes)):
     if answer < ropes[0]*len(ropes):
         answer = ropes[0]*len(ropes)
-        heapq.heappop(ropes)
+    heapq.heappop(ropes)
+    #print(ropes)
 
 print(answer)
