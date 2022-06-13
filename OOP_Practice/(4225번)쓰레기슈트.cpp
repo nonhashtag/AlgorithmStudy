fadf88ca3cd56341bbf5fba4260983e93e00aa35
nonhashtag <75101880+nonhashtag@users.noreply.github.com>
2022-06-14 01:31:14 +0900

    Update (4225번)쓰레기슈트.cpp

--- OOP_Practice/(4225번)쓰레기슈트.cpp
+++ OOP_Practice/(4225번)쓰레기슈트.cpp
@@ -26,6 +26,28 @@ int ccw(Point p1, Point p2, Point p3)
 		return CounterClockWise;
 }
 
+int getDistance(Point p1, Point p2)
+{
+	return (p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y);
+}
+
+bool ccwCompare(const Point& starting_point, const Point& p1, const Point& p2)
+{
+	int z = ccw(starting_point, p1, p2);
+	if (z > 0)
+		return true;
+	if (z < 0)
+		return false;
+	
+	return getDistance(starting_point, p1) < getDistance(starting_point, p2);
+}
+
+bool startingPointCompare(const Point& p1, const Point& p2)
+{
+	auto zero_point = Point(0, 0);
+	return getDistance(zero_point, p1) < getDistance(zero_point, p2);
+}
+
 class GarbageChute
 {
 private:
@@ -36,15 +58,29 @@ public:
 
 };
 
-class ConvexHall
+class ConvexHull
 {
 private:
-
+	std::vector<Point> dots;
 
 public:
-
+	std::vector<Point> graham();
 };
 
+
+std::vector<Point> graham(std::vector<Point> dots)
+{
+	std::vector <Point> verts;
+	for (int i = 0; i < dots.size(); i++)
+	{
+		while (2 <= verts.size() && ccw(verts[verts.size() - 2], verts[verts.size() - 1], dots[i]) <= 0)
+		{
+			verts.pop_back();
+		}
+		verts.push_back(dots[i]);
+	}
+	return verts;
+}
 
 int main()
 {
